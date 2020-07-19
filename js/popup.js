6438987cf951d25cccb4478c29a454ef3408ea89
rishav12 <rishavqwerty7@gmail.com>
2020-07-19 07:16:01 +0530

    jsfile added

--- js/popup.js
+++ js/popup.js
@@ -1,3 +1,20 @@
+const tabs = document.querySelectorAll('[data-tab-target]')
+const tabContents = document.querySelectorAll('[data-tab-content]')
+
+tabs.forEach((tab) => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.tabTarget)
+    tabContents.forEach((tabContent) => {
+      tabContent.classList.remove('active')
+    })
+    tabs.forEach((tab) => {
+      tab.classList.remove('active')
+    })
+    tab.classList.add('active')
+    target.classList.add('active')
+  })
+})
+
 let data = []
 let pageData = []
 let perPage = 10
@@ -23,7 +40,7 @@ function loadData() {
 
 function pagination(page) {
   let total = data.length
-  let pageCOunt = 9
+  let pageCOunt = 8
   let pages = document.getElementById('pages')
   pages.innerHTML = ''
 
@@ -63,12 +80,12 @@ function fillPage() {
 
   pageData.forEach((item) => {
     const singleData = ` <tr>
-            <td class="border" style="color:white">${item.Country}</td>
+            <td class="border" style="color:white" >${item.Country}</td>
             <td class="border" style="color:orange">${item.TotalConfirmed}</td>
-            <td class="border" style="color:blue">${item.NewConfirmed}</td>
+            <td class="border" style="color:#00FFFF">${item.NewConfirmed}</td>
             <td class="border" style="color:red">${item.TotalDeaths}</td>
          
-            <td class="border" style="color:rgb(94, 231, 76)">${item.TotalRecovered}</td>
+            <td class="border" style="color:#00FF00">${item.TotalRecovered}</td>
           </tr>`
     div.insertAdjacentHTML('beforeend', singleData)
   })
@@ -76,4 +93,50 @@ function fillPage() {
 
 window.addEventListener('load', () => {
   pagination(activePage)
+})
+
+fetch('https://thevirustracker.com/free-api?global=stats')
+  .then((response) => response.json())
+  .then((res) => {
+    console.log(res)
+    var tot = document.getElementById('tota')
+    var h1 = document.createElement('h4')
+    h1.style.color = '#00FFFF'
+    h1.textContent = res.results[0].total_cases
+    h1.style.marginLeft = '35px'
+    h1.style.marginRight = '25px'
+    h1.style.cssFloat = 'left'
+    var h2 = document.createElement('h4')
+    h2.style.color = 'orange'
+    h2.textContent = res.results[0].total_unresolved
+    tot.append(h1, h2)
+    var tot2 = document.getElementById('tota2')
+    var h3 = document.createElement('h4')
+    h3.style.color = '#00FF00'
+    h3.textContent = res.results[0].total_recovered
+    h3.style.marginLeft = '45px'
+    h3.style.marginRight = '35px'
+    h3.style.cssFloat = 'left'
+    var h4 = document.createElement('h4')
+    h4.style.color = 'red'
+    h4.textContent = res.results[0].total_deaths
+
+    tot2.append(h3, h4)
+  })
+
+//function for search
+document.getElementById('searching').addEventListener('click', function () {
+  document.getElementById('global').innerHTML = ''
+  var search = document.getElementById('searching').value
+  fetch('https://api.covid19api.com/live/country/south-africa/status/confirmed')
+    .then((response) => response.json())
+    .then((res) => {
+      var glob = document.getElementById('global')
+      var h6 = document.createElement('h6')
+      h6.style.textAlign = 'center'
+      h6.style.color = '#00FF00'
+      h6.style.marginLeft = '40px'
+      h6.textContent = 'Cases:' + res[0].Confirmed
+      glob.append(h6)
+    })
 })
